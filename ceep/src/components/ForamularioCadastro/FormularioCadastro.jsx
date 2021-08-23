bf5ab2fb456b742f822cdc698253f019cdc59ceb
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2021-08-23 16:55:11 -0300

    organizacao

--- ceep/src/components/ForamularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/ForamularioCadastro/FormularioCadastro.jsx
@@ -1,23 +1,36 @@
 import React, { Component } from "react"
 import "./FormularioCadastro.css"
 
-class FormularioCadastro extends Component{
+class FormularioCadastro extends Component {
 
-    render(){
-        return(
-        <form className="form-cadastro">
-            <input 
-                className="form-cadastro_input" 
-                type='text' 
-                placeholder="Título">     
-            </input>
-            <textarea 
-                rows={15} 
-                className="form-cadastro_input" 
-                placeholder="escreva sua nota">
-            </textarea>
-            <button className="form-cadastro_submit">Criar Nota</button>
-        </form> 
+    constructor() {
+        super();
+        this._titulo = "";
+    }
+
+    handleMudancaDeTitulo(event) {
+        this._titulo = event.target.value;
+
+        console.log(this._titulo);
+    }
+
+    render() {
+        return (
+            <form className="form-cadastro">
+                <input
+                    className="form-cadastro_input"
+                    type='text'
+                    placeholder="Título"
+                    onChange={this.handleMudancaDeTitulo.bind(this)}
+                >
+                </input>
+                <textarea
+                    rows={15}
+                    className="form-cadastro_input"
+                    placeholder="escreva sua nota">
+                </textarea>
+                <button className="form-cadastro_submit">Criar Nota</button>
+            </form>
         );
     }
 }
